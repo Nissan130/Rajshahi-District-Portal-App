2c3bae527d6b5a1c018078075337f3dba53f1672
nissan <mdnissanali130@gmail.com>
2025-06-27 13:04:50 +0600

    changes saved

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://192.168.43.232:3000/api", // mobile
-  baseURL: "http://10.1.1.108:3000/api",
+  // baseURL: "http://10.1.1.108:3000/api",
+  baseURL: "https://rajshahi-district-portal-app-server.onrender.com/api",
   headers: {
     "Content-Type": "multipart/form-data",
   },
